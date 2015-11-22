f42cdf47bcb719409383d385d83c15c5b9e6872e
elc1798 <subliminalmau5@gmail.com>
2015-11-21 21:52:29 -0500

    Organize includes

--- utils/prompt.c
+++ utils/prompt.c
@@ -1,18 +1,19 @@
-#ifndef _WISH_PROMPT
-#define _WISH_PROMPT
-
+// Standard includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/wait.h>
-#include "ansi_colors.h"
+#include <errno.h>
 
-char *PROMPT;
-char **PROMPT_VARS;
-char *RPROMPT;
-char **RPROMPT_VARS;
+// Includes for fancy things
+#include <pwd.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+// Includes for wish header files
+#include "ansi_colors.h"
+#include "prompt.h"
 
 void load_prompt_defaults() {
     *PROMPT = "%s@%s (%s):$ ";
